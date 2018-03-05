2ac2afe5c2d4708591b4f864513963767b49631a
ohduran <alvaro.duranb@gmail.com>
2018-03-05 21:31:20 +0000

    2.14

---
+++ 2 - Strings and Text/2.14 - Combining and concatenating strings.py
@@ -0,0 +1,9 @@
+#!environment/bin/python3
+"""
+Combine many small strings together into a larger string.
+"""
+
+# join() methods
+parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+
+print(' '.join(parts)) # Is Chicago Not Chicago?
